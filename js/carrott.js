9fd98832b2e84db011eb9d67db90d0c6a00f12f6
aubreyalfonzo <aubrey.j.pohl@gmail.com>
2022-07-12 20:14:27 -0500

    update

--- js/carrott.js
+++ js/carrott.js
@@ -1,10 +1,18 @@
-let img, img2;
+let img;
 let w,h;
 
+let veg = [];
+let index = 0;
+
 function preload(){
+
+	veg[0] = loadImage("https://i.imgur.com/bDcAZkZ.png");
+	veg[1] = loadImage("https://i.imgur.com/GKE3MUM.png");
+	veg[2] = loadImage("https://i.imgur.com/D9yyVk1.png");
 	
-	img = loadImage("https://i.imgur.com/Peql1fE.png");
-	img2 = loadImage("https://i.imgur.com/NxSgvxi.png");
+
+	img = loadImage("https://i.imgur.com/NxSgvxi.png");
+
 }
 
 function setup() {
@@ -13,36 +21,46 @@ function setup() {
 	createCanvas(windowWidth, windowHeight);
 	imageMode(CENTER);
 
-	img2.resize(width/3, width/3);
+	img.resize(width/3, width/3);
 
 	w = width/2;
-	h = width/2;
+	h = height/2;
 	
 }
 
 function draw() {
-	background(147, 39, 143, 5);
+	background(147, 39, 143, 10);
+	// background(255, 10);
 
 	
 	// translate(mouseX, mouseY);
 	w = sin(frameCount*.0005)*width;
 	h = sin(frameCount*.0005)*height;
 
+	if(mouseX > 0){
+		w = mouseX;
+		h = mouseY;
+	}
+
 	push();
+
 	translate(w, h);
 
-	// if(mouseX > -1){
-	// 	w = mouseX;
-	// 	h = mouseY;
-	// }
-
-	rotate(radians(frameCount*.5));
-	image(img, 0, 0);
+	rotate(radians(frameCount*.08));
+	image(veg[index], 0, 0);
 	pop();
 
 	push();
 	translate(width/2, height/2);
-	image(img2, 0, 0);
+	image(img, 0, 0);
 	pop();
 	
+}
+
+function mousePressed() {
+  index = index + 1;
+  
+  if(index == veg.length){
+   	index = 0; 
+  }
 }
